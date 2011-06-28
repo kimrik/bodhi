fee26540b04c7ef92bdb69184fdfe37dac1c4474
kimrik <vovcheg.k@gmail.com>
2011-06-29 00:32:25 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bodhi::Application.config.secret_token = 'ef027d88a8f556875d8826675f1a8909a1caf81051fb00b4e0c6ca9770568bd0e8dee9df0db33c2ca75a596587f155ae818f8a26fa70f51909df17e4dce7aac9'
